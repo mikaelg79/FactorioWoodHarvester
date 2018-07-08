8b9603767b6b666ae0b65fdb293ed727bfdee51e
mikaelg79 <mikael.gunnarsson@gmail.com>
2018-07-08 18:03:54 +0200

    0.0.5 - Now with WORKING migrations.

--- migrations/migrations.lua
+++ migrations/migrations.lua
@@ -2,5 +2,5 @@
 
 for index,force in pairs(game.forces) do
     force.recipes["wood-harvester"].enabled = force.technologies["automation"].researched
-    force.recipes.reload()
+    force.recipes["wood-harvester"].reload()
 end
